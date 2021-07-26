7e1deb293eea372bdec125fa34eba5f3f701a0aa
burstknight <burstknight@gmail.com>
2021-07-26 13:48:58 +0800

    Update test code to debug
    Test if flask_classful repeats call constructor of the
    subclass of FlaskView .

--- testRepeatConstuctorError.py
+++ testRepeatConstuctorError.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_classful import FlaskView, route
+from random import randint
 
 app = Flask(__name__)
 
@@ -7,23 +8,24 @@ class AppRouting(FlaskView):
 	route_base = '/'
 
 	def __init__(self):
-		print("--------constructor---------")
+		self.m_iData = randint(0, 9999)
+		print("--------constructor: %d ---------" %(self.m_iData))
 		return 
 
 	def test1(self):
-		return "test1"
+		return "test1 " + str(self.m_iData)
 
 	def test2(self):	
-		return "test2"
+		return "test2 " + str(self.m_iData)
 
 	def test3(self):
-		return "test3"
+		return "test3 " + str(self.m_iData)
 
 	def test4(self):
-		return "test4"
+		return "test4 " + str(self.m_iData)
 
 	def test5(self):
-		return "test5"
+		return "test5 " + str(self.m_iData)
 
 
 if __name__ == '__main__':
